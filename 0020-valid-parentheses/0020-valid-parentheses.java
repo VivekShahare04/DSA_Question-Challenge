ffe537aede9198c10c61c6230ecbdb12078aa3d1
VivekShahare04 <138312154+VivekShahare04@users.noreply.github.com>
2024-07-01 07:56:58 +0530

    Time: 1 ms (98.95%), Space: 41.4 MB (54.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-        
+       Stack<Character> st = new Stack<>();
+       for(char c:s.toCharArray()){
+           if(c=='('){
+               st.push(')');
+           }
+            else if(c=='{'){
+            st.push('}');
+            }
+            else if(c=='['){
+            st.push(']');
+            }
+            else if(st.isEmpty() || st.pop()!=c){
+            return false;  
+            }
+            // else{
+            //     return true;
+            // }
+       }
+       return st.isEmpty();
     }
 }
