496477370b62f2e9eb2c1f7ff5e113fd407be1e5
VivekShahare04 <138312154+VivekShahare04@users.noreply.github.com>
2025-01-21 22:08:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class GFG {
+    ArrayList<Integer> find(int arr[], int x) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int low=0;
+        int high=arr.length-1;
+        list.add(-1);
+        list.add(-1);
+    
+        
+        while(low<=high){
+            if(arr[low]==x && arr[high]==x){
+                list.add(0,low);
+                list.add(1,high);
+                break;
+            }
+            if(arr[high]!=x){
+                high--;
+            }
+            if(arr[low]!=x){low++;}
+            
+        }
+        return list;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int n = a1.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(a1[i]);
+            }
+            int x = Integer.parseInt(br.readLine());
+            GFG ob = new GFG();
+            ArrayList<Integer> ans = ob.find(arr, x);
+            System.out.println(ans.get(0) + " " + ans.get(1));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
