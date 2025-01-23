7e10a30bb1af0c3d53b5899cfcbd60ffc33e4056
VivekShahare04 <138312154+VivekShahare04@users.noreply.github.com>
2025-01-23 22:50:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two sum -Pairs with 0 Sum/two-sum-pairs-with-0-sum.java
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class IntMatrix {
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException {
+        int[][] mat = new int[n][];
+
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for (int j = 0; j < s.length; j++) mat[i][j] = Integer.parseInt(s[j]);
+        }
+
+        return mat;
+    }
+
+    public static void print(int[][] m) {
+        for (var a : m) {
+            for (int e : a) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+
+    public static void print(ArrayList<ArrayList<Integer>> m) {
+        for (var a : m) {
+            for (int e : a) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer>> res = obj.getPairs(arr);
+            if (res.size() == 0) {
+                System.out.println();
+            } else {
+                IntMatrix.print(res);
+            }
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> getPairs(int[] arr) {
+        
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<String> pairs = new HashSet<>();
+        
+        for(int num:arr){
+            int comp = -num;
+            if(seen.contains(comp)){
+                int min = Math.min(num,comp);
+                int max = Math.max(num,comp);
+                String pairkeys = min + "," + max;
+                if(!pairs.contains(pairkeys)){
+                    res.add(new ArrayList<>(Arrays.asList(min,max)));
+                    pairs.add(pairkeys);
+                }
+            }
+            seen.add(num);
+        }
+        res.sort((a,b)->{
+            if(a.get(0).equals(b.get(0))){
+                return a.get(1)-b.get(1);
+            }
+            return a.get(0)-b.get(0);
+        });
+        
+        return res;
+    }
+}
