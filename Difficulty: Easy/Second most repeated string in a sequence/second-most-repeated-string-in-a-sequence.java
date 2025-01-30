f738bb9c2724bbdc0db4eaa3315ecee7728bf05c
VivekShahare04 <138312154+VivekShahare04@users.noreply.github.com>
2025-01-30 23:44:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second most repeated string in a sequence/second-most-repeated-string-in-a-sequence.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            String arr[] = new String[n];
+            for(int i = 0;i<n;i++)
+                arr[i] = sc.next();
+            
+            Solution ob = new Solution();
+            
+            System.out.println(ob.secFrequent(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    String secFrequent(String arr[], int N)
+    {
+        HashMap<String,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        
+        List<Map.Entry<String,Integer>> ls = new ArrayList<>(map.entrySet());
+        ls.sort((a,b)->b.getValue()-a.getValue());
+        
+        if(ls.size()>1){
+            return ls.get(1).getKey();
+        }
+        return "";
+    }
+}
